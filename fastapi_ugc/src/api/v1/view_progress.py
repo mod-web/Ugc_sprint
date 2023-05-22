30942b4fcd57977ab6707dd8ccf9141cbede5d40
dmitriyfedotov <fedotovdmitriy14@gmail.com>
2023-05-23 01:31:17 +0300

    added initial version of save_view_progress_to_kafka endpoint

--- fastapi_ugc/src/api/v1/view_progress.py
+++ fastapi_ugc/src/api/v1/view_progress.py
@@ -1,4 +1,16 @@
 from fastapi import APIRouter
 
+from src.models.view_progress import QueryParams
 
 router = APIRouter()
+
+
+@router.post(
+    '/save',
+    description='Save view progress to kafka',
+    summary='Save view progress to kafka',
+)
+async def save_view_progress_to_kafka(
+    save_view_progress_to_kafka_params: QueryParams,
+):
+    return {'res': save_view_progress_to_kafka_params.dict()}
