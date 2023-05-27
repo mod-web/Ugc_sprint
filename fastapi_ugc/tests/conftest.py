39bb36808495bd0b3885553fbd8c6409c685c593
Александр Карулин <av.karulin@tensor.ru>
2023-05-27 15:56:56 +0300

    Send event to kafka

--- fastapi_ugc/tests/conftest.py
+++ fastapi_ugc/tests/conftest.py
@@ -1,4 +1,19 @@
-HOST = 'http://0.0.0.0'
+import pytest
+from kafka.admin import KafkaAdminClient
+
+HOST = '0.0.0.0'
+PORT = '8000'
 USER_ID = '1569523'
 FILM_ID = '51554'
 VIEWED_FRAME = 340
+KAFKA_TEST_TOPIC = 'test_mviews'
+KAFKA_PORT = '9092'
+
+
+@pytest.fixture
+def create_and_delete_test_topic():
+    """Удалить тестовый топик"""
+    kafka_admin = KafkaAdminClient(bootstrap_servers=f'{HOST}:{KAFKA_PORT}')
+    kafka_admin.create_topics((KAFKA_TEST_TOPIC,))
+    yield
+    KafkaAdminClient(bootstrap_servers=f'{HOST}:{KAFKA_PORT}').delete_topics((KAFKA_TEST_TOPIC,))
--- fastapi_ugc/tests/test_save_progress.py
+++ fastapi_ugc/tests/test_save_progress.py
@@ -1,19 +1,59 @@
+import json
+from time import sleep
+
 import requests
 from datetime import datetime
-from conftest import HOST, USER_ID, FILM_ID, VIEWED_FRAME
 
+from kafka import KafkaProducer, KafkaConsumer
 
-def test_save_movie_progress():
-    headers = {"Content-Type": "application/json; charset=utf-8"}
-    url = f'{HOST}/api/v1/save'
-    data = {
+from tests.conftest import HOST, PORT, USER_ID, FILM_ID, VIEWED_FRAME, KAFKA_TEST_TOPIC, KAFKA_PORT
+
+data = {
             'user_id': USER_ID,
             'film_id': FILM_ID,
             'viewed_frame': VIEWED_FRAME,
             'message_time': str(datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')),
         }
+
+
+def test_save_movie_progress():
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = f'http://{HOST}:{PORT}/api/v1/view_progress/save'
     response = requests.post(url=url,
                              json=data,
                              headers=headers
                              )
     assert response.status_code == 200
+
+
+def test_send_event_to_topic(create_and_delete_test_topic):
+    """Проверка отправки события в Kafka"""
+
+    producer = KafkaProducer(bootstrap_servers=f'{HOST}:{KAFKA_PORT}')
+    producer.send(
+            key=f'{data["user_id"]}:{data["film_id"]}'.encode('utf-8'),
+            topic=KAFKA_TEST_TOPIC,
+            value=json.dumps(data).encode('utf-8'),
+        )
+    sleep(1)
+    consumer = KafkaConsumer(
+        KAFKA_TEST_TOPIC,
+        bootstrap_servers=[f'{HOST}:{KAFKA_PORT}'],
+        auto_offset_reset='earliest',
+        group_id='echo-messages-to-stdout',
+    )
+    kafka_data = consumer.next().value
+    for field_name, field_value in data.items():
+        assert kafka_data[field_name] == field_value
+
+
+# def test_load_message_to_clickhouse(create_and_delete_test_topic):
+#     """Проверка загрузки сообщения в ClickHouse"""
+#
+#     producer = KafkaProducer(bootstrap_servers=f'{HOST}:{KAFKA_PORT}')
+#     producer.send(
+#             key=f'{data["user_id"]}:{data["film_id"]}'.encode('utf-8'),
+#             topic=KAFKA_TEST_TOPIC,
+#             value=json.dumps(data).encode('utf-8'),
+#         )
+#     sleep(1)
