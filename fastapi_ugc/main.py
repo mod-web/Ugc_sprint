b6bf62d49d2d9196e3878befc80cef62122ecfa3
dmitriyfedotov <fedotovdmitriy14@gmail.com>
2023-05-28 23:33:13 +0300

    fixed kafka producer

--- fastapi_ugc/main.py
+++ fastapi_ugc/main.py
@@ -1,10 +1,14 @@
+import asyncio
+
 import uvicorn
+from aiokafka import AIOKafkaProducer
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
 
 from settings import settings
 from src.api.v1 import view_progress
+from src.services import kafka_storage
 
 app = FastAPI(
     title=settings.project_name,
@@ -15,6 +19,24 @@ app = FastAPI(
 
 
 app.include_router(view_progress.router, prefix='/api/v1/view_progress', tags=['view_progress'])
+
+
+@app.on_event('startup')
+async def startup():
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop = asyncio.get_event_loop()
+    producer = AIOKafkaProducer(loop=loop, bootstrap_servers=f'{settings.kafka_host}:{settings.kafka_port}')
+    kafka_storage.kafka_producer = producer
+    await kafka_storage.kafka_producer.start()
+
+
+@app.on_event('shutdown')
+async def shutdown():
+    await kafka_storage.kafka_producer.stop()
 
 
 if __name__ == '__main__':
