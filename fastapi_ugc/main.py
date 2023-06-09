a3100d14fdebd7752324b159c125099314cdaf40
Александр Карулин <av.karulin@tensor.ru>
2023-06-09 21:58:07 +0300

    fix logstash

--- fastapi_ugc/main.py
+++ fastapi_ugc/main.py
@@ -1,15 +1,17 @@
 import asyncio
+import logging
 import os
 
 import sentry_sdk
 import uvicorn
 from aiokafka import AIOKafkaProducer
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import ORJSONResponse
 
 from settings import settings
 from src.api.v1 import view_progress
 from src.services import kafka_storage
+import logstash
 
 app = FastAPI(
     title=settings.project_name,
@@ -18,8 +20,20 @@ app = FastAPI(
     default_response_class=ORJSONResponse,
 )
 
+app.logger = logging.getLogger(__name__)
 
 app.include_router(view_progress.router, prefix='/api/v1/view_progress', tags=['view_progress'])
+
+
+@app.middleware("http")
+async def log_middle(request: Request, call_next):
+    if 'X-Request-Id' in request.headers:
+        request_id = request.headers.get('X-Request-Id')
+    else:
+        request_id = None
+    app.logger.info(f'request_id {request_id}')
+    response = await call_next(request)
+    return response
 
 
 @app.on_event('startup')
@@ -34,6 +48,14 @@ async def startup():
     producer = AIOKafkaProducer(loop=loop, bootstrap_servers=f'{settings.kafka_host}:{settings.kafka_port}')
     kafka_storage.kafka_producer = producer
     await kafka_storage.kafka_producer.start()
+    app.logger.setLevel(logging.INFO)
+    logstash_handler = logstash.LogstashHandler('elk-logstash', 5044, version=1)
+    app.logger.addHandler(logstash_handler)
+
+
+@app.route('/debug-sentry')
+def trigger_error():
+    division_by_zero = 1 / 0
 
 
 @app.on_event('shutdown')
