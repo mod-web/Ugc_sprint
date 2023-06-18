969ca069f2c822aed5dbedc9bd6b1ffd9890044c
dmitriyfedotov <fedotovdmitriy14@gmail.com>
2023-06-18 15:33:21 +0300

    fixed mypy

--- fastapi_ugc/src/models/bookmark.py
+++ fastapi_ugc/src/models/bookmark.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 
 from pydantic import BaseModel, validator
 
@@ -6,7 +7,7 @@ from pydantic import BaseModel, validator
 class BookMark(BaseModel):
     film_id: str
     user_id: str
-    created: datetime | None
+    created: Optional(datetime)
 
     @validator('created', pre=True, always=True)
     def set_created(cls, v):  # noqa: N805
