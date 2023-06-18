b324521ffb4630cae2085aabc1fba485ab62ddb1
dmitriyfedotov <fedotovdmitriy14@gmail.com>
2023-06-19 00:02:14 +0300

    added indexes and removed review_likes collection

--- etc/mongo/init.js
+++ etc/mongo/init.js
@@ -4,4 +4,7 @@ db = conn.getDB('ugc_movies');
 db.createCollection('likes');
 db.createCollection('bookmarks');
 db.createCollection('reviews');
-db.createCollection('review_likes');
+
+db.likes.createIndex({ film_id: 1, user_id: 1 }, { unique: true });
+db.bookmarks.createIndex({ film_id: 1, user_id: 1 }, { unique: true });
+db.reviews.createIndex({ film_id: 1, user_id: 1 }, { unique: true });
